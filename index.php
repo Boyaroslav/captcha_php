83fe895d08558b6233f08b7a044310b39293bba6
zhorik <76032531+Boyaroslav@users.noreply.github.com>
2023-08-28 17:55:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	}
 	function submit(){
 		$value =  $_POST['answer'];
-		
+
 		if ($value == file_get_contents("buff.txt")){
 			echo '100 баллов';
 		}
@@ -20,6 +20,10 @@
 	putenv('GDFONTPATH=' . realpath('.'));
 	$im = imagecreatetruecolor(150, 25);
 	$font = imageloadfont('./teletext.gdf');
+	$font2 = imageloadfont('./latin.gdf');
+	$font3 = imageloadfont('./nsw.gdf');
+	$font4 = imageloadfont('./fbold.gdf');
+	$farr = array($font, $font2, $font3, $font4);
 	$bg = imagecolorallocate($im,200,255,200);
 	imagefilledrectangle($im, 0, 0, 200, 50, $bg);
 
@@ -31,7 +35,7 @@
 		$g = rand(0, 100);
 		$b = rand(0, 100);
 		$bg1 = imagecolorallocate($im, $r, $g, $b);
-		imagestring($im, $font, $i * 10 + 20, rand(0, 10),  $buf, $bg1);
+		imagestring($im, $farr[rand(0, 3)], $i * 10 + 20, rand(0, 10),  $buf, $bg1);
 	}
 
 	file_put_contents("buff.txt", $ans);
